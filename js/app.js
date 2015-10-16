328e8b14a3ceba98cf1f09f7f97fdae71649d540
Daniel Silva <you@example.com>
2015-10-16 16:31:12 +0100

    daniel

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
 
 $(document).ready(function(){
+
+	var randomVal = Math.floor((Math.random() * 100) + 1);
 	
 	/*--- Display information modal box ---*/
   	$(".what").click(function(){
@@ -12,6 +14,58 @@ $(document).ready(function(){
   		$(".overlay").fadeOut(1000);
   	});
 
+  	$(".new").click(function(){
+
+
+  		randomVal = Math.floor((Math.random() * 100) + 1);
+  	});
+
+
+
+  	$("#guessButton").click(function(e){
+
+  		var insertedVal = parseInt(document.getElementById('userGuess').value);
+
+  		if(randomVal == insertedVal){
+  			
+  			$("#feedback").text("Nailed It !");
+  			
+  			randomVal = Math.floor((Math.random() * 100) + 1);
+  			e.preventDefault();
+  		}
+
+  		else if((randomVal + 1 < insertedVal && randomVal + 10 > insertedVal) || (insertedVal + 1 < randomVal && insertedVal + 10 > randomVal) ){
+
+  			$("#feedback").text("Very Hot");
+  			
+  			e.preventDefault();
+  		}else if((randomVal + 10  < insertedVal && randomVal + 20 > insertedVal)  || (insertedVal + 10 < randomVal && insertedVal + 20 > randomVal) ){
+
+  			$("#feedback").text("Hot");
+  		
+  			e.preventDefault();
+
+  		} else if((randomVal + 20  < insertedVal && randomVal + 30 > insertedVal)  || (insertedVal + 20 < randomVal && insertedVal + 30 > randomVal) ){
+
+  			$("#feedback").text("Warm");
+  			
+  			e.preventDefault();
+  		} else if((randomVal + 30  < insertedVal && randomVal + 50 > insertedVal)  || (insertedVal + 30 < randomVal && insertedVal + 50 > randomVal) ){
+
+  			$("#feedback").text("Cold");
+  			
+  			e.preventDefault();
+  		}
+  		 else if((randomVal + 50  < insertedVal && randomVal + 100 > insertedVal)  || (insertedVal + 50 < randomVal && insertedVal + 100 > randomVal) ){
+
+  			$("#feedback").text("Ice Cold");
+  			
+  			e.preventDefault();
+  		}
+  	});
 });
+
+
+
 
 
